9609c09781d78c63a5d14d4b4084e9ec819289f1
catuxian <15926gg@gmail.com>
2021-02-02 16:24:01 +0800

    完成主畫面

---
+++ front/login.php
@@ -0,0 +1,49 @@
+<fieldset>
+    <legend>會員登入</legend>
+    <form>
+        <table>
+            <tr>
+                <td>帳號</td>
+                <td><input type="text" name="acc" id="acc"></td>
+            </tr>
+            <tr>
+                <td>密碼</td>
+                <td><input type="text" name="pw" id="pw"></td>
+            </tr>
+            <tr>
+                <td>
+                    <input type="button" value="登入"><input type="reset" value="清除">
+                </td>
+                <td>
+                    <a href="?do=forget">忘記密碼</a>|<a href="?do=reg">尚未註冊</a>
+                </td>
+            </tr>
+        </table>
+    </form>
+</fieldset>
+
+<script>
+    function login(){
+        let acc=$("#acc").val();
+        let pw=$("#pw").val();
+        $.get("api/chk_acc.php",{acc},(re)=>{
+            if(re=='1'){
+                $.get("api/chk_pw.php",{acc,pw},(re)=>{
+                    if(re=='1'){
+                        if(acc=='admin'){
+                            location.href="admin.php";
+                        }else{
+                            location.href="index.php";
+                        }
+                    }else{
+                        alert("密碼錯誤");
+                        location.reload();
+                    }
+                })
+            }else{
+                alert("查無帳號");
+                location.reload();
+            }
+        })
+    }
+</script>
